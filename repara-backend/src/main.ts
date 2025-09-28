84a28f5f15763b204c84c51628002dcfc289a3a8
AlcidesAT <alcidesantonio08@hotmail.com>
2025-09-28 19:22:56 -0300

    cors

--- repara-backend/src/main.ts
+++ repara-backend/src/main.ts
@@ -6,10 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
 const app = await NestFactory.create(AppModule);
 app.enableCors({ 
-  origin: [
-    /\.vercel\.app$/,
-    /\.onrender\.com$/
-  ] 
+    origin: ['https://repara-front1.vercel.app']
 });
 app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 const port = process.env.PORT || 3001;
