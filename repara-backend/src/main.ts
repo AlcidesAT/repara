376729cf4fa862c187ef71b49a8f5b60381474b6
AlcidesAT <alcidesantonio08@hotmail.com>
2025-09-28 20:32:09 -0300

    fix url

--- repara-backend/src/main.ts
+++ repara-backend/src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
 const app = await NestFactory.create(AppModule);
 app.enableCors({ 
-    origin: ['https://repara-front.vercel.app/']
+    origin: ['https://repara-front.vercel.app']
 });
 app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 const port = process.env.PORT || 3001;
