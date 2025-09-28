a8fdac4d5d37e216f71dc97b64f4d57e1b800dc5
AlcidesAT <alcidesantonio08@hotmail.com>
2025-09-28 19:10:45 -0300

    fix: cors

--- repara-backend/src/main.ts
+++ repara-backend/src/main.ts
@@ -7,8 +7,6 @@ async function bootstrap() {
 const app = await NestFactory.create(AppModule);
 app.enableCors({ 
   origin: [
-    /localhost:3000$/, 
-    /localhost:3001$/,
     /\.vercel\.app$/,
     /\.onrender\.com$/
   ] 
